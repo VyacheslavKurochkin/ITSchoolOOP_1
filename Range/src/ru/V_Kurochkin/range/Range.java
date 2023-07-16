a2602d6760c8a3c6a71cf03098d59519c9dcf71c
V_Kurochkin <v.kurochkin1269@gmail.com>
2023-07-16 13:02:55 +0700

    Create class Range (*)

--- Range/src/ru/V_Kurochkin/range/Range.java
+++ Range/src/ru/V_Kurochkin/range/Range.java
@@ -1,5 +1,90 @@
 package ru.V_Kurochkin.range;
 
 public class Range {
+    private double from;
+    private double to;
 
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && to >= number;
+    }
+
+    public Range getIntersection(Range range) {
+
+        if (this.to > range.getFrom() && this.from < range.getTo()) {
+            if (range.getFrom() > this.from && range.getTo() < this.to) {
+                return new Range(range.getFrom(), range.getTo());
+            }
+
+            if (range.getFrom() <= this.from && range.getTo() >= this.to) {
+                return new Range(this.from, this.to);
+            }
+
+            if (this.from > range.getTo() && range.getTo() < this.to) {
+                return new Range(this.from, range.getTo());
+            }
+
+            return new Range(range.getFrom(), this.to);
+        }
+
+        return null;
+    }
+
+    public Range[] getUnion(Range range) {
+
+        if (this.to >= range.getFrom() && this.from <= range.getTo()) {
+            double newRangeFrom = (this.from < range.getFrom()) ? this.from : range.getFrom();
+            double newRangeTo = (this.to > range.getTo()) ? this.to : range.getTo();
+
+            return new Range[]{new Range(newRangeFrom, newRangeTo)};
+        }
+
+        return new Range[]{new Range(this.from, this.to), new Range(range.getFrom(), range.getTo())};
+    }
+
+    public Range[] getSubtraction(Range range) {
+
+        if (this.to > range.getFrom() && this.from < range.getTo()) {
+            if (range.getFrom() > this.from && range.getTo() < this.to) {
+                return new Range[]{new Range(this.from, range.getFrom()), new Range(range.getTo(), this.to)};
+            }
+
+            if (range.getFrom() <= this.from && range.getTo() >= this.to) {
+                return null;
+            }
+
+            if (this.from > range.getTo() && range.getTo() < this.to) {
+                return new Range[]{new Range(range.getTo(), this.to)};
+            }
+
+            return new Range[]{new Range(this.from, range.getFrom())};
+        }
+
+        return new Range[]{new Range(this.from, this.to)};
+    }
 }
+
--- Range/src/ru/V_Kurochkin/range_main/Main.java
+++ Range/src/ru/V_Kurochkin/range_main/Main.java
@@ -1,7 +1,46 @@
 package ru.V_Kurochkin.range_main;
 
+import ru.V_Kurochkin.range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range1 = new Range(2.1, 2.5);
+        Range range2 = new Range(2.2, 2.8);
+
+        System.out.println("Диапазоны: ");
+        System.out.printf("[%.2f .. %.2f] \r\n", range1.getFrom(), range1.getTo());
+        System.out.printf("[%.2f .. %.2f] \r\n", range2.getFrom(), range2.getTo());
+
+        Range[] rangeUnion = range1.getUnion(range2);
+
+        System.out.println("Объединение: ");
+
+        for (int i = 0; i < rangeUnion.length; i++) {
+            System.out.printf("%d : ", i + 1);
+            System.out.printf("[%.2f .. %.2f]\r\n", rangeUnion[i].getFrom(), rangeUnion[i].getTo());
+        }
+
+        Range[] rangeSubtraction = range1.getSubtraction(range2);
+
+        System.out.println("Разность: ");
+
+        if (rangeSubtraction != null) {
+            for (int i = 0; i < rangeSubtraction.length; i++) {
+                System.out.printf("%d : ", i + 1);
+                System.out.printf("[%.2f .. %.2f]\r\n", rangeSubtraction[i].getFrom(), rangeSubtraction[i].getTo());
+            }
+        } else {
+            System.out.println("Пустой диапазон");
+        }
+
+        Range rangeIntersection = range1.getIntersection(range2);
+
+        System.out.println("Пересечение: ");
+
+        if (rangeIntersection != null) {
+            System.out.printf("[%.2f .. %.2f]\r\n", rangeIntersection.getFrom(), rangeIntersection.getTo());
+        } else {
+            System.out.println("Пустой диапазон");
+        }
     }
 }
